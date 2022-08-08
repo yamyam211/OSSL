4feed331e8aa6d6c259fa3ba071beaee3857347c
yamyam211 <110814653+yamyam211@users.noreply.github.com>
2022-08-08 13:56:26 +0530

    Add files via upload

---
+++ Q2.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+using namespace std;
+
+int main()
+{
+    cout<<"This is a program for OSSL Week 1 Q2.";
+    int x;
+    cout<<"Enter a number: "; cin>>x;
+    cout<<"You entered "<<x;
+
+    return 0;
+}
